0d2225d149d457a7873b52cabaf7123b1ebf72c5
Krzysztof Stefański <krzysztof.stefanski82@gmail.com>
2018-08-04 16:59:49 +0200

    handleClick BoardClass

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
-
-
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-
-
+import './index.css'
 
 
 class Square extends React.Component {
-constructor(props){
-    super(props)
-    this.state={
-        value:null
-    }
-}
-
     render() {
       return (
-        <button className="square" onClick={()=> alert('click')}>
+        <button
+          className="square"
+          onClick={() => this.props.onClick()}
+        >
           {this.props.value}
         </button>
       );
@@ -25,8 +17,26 @@ constructor(props){
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+  
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+  
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
@@ -36,19 +46,13 @@ constructor(props){
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
           </div>
         </div>
       );
@@ -77,4 +81,3 @@ constructor(props){
     <Game />,
     document.getElementById('root')
   );
-  
